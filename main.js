a7deda9e33ab433df6e82c9d738fc892d2292d3d
dlinda <linda.debray@outlook.com>
2019-12-16 11:26:08 +0100

    changed code from vue root element to use vue components, added a premium property to varify if shippment is free for user or not

--- main.js
+++ main.js
@@ -1,38 +1,96 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    brand: 'Lindas',
-    product: 'Flower Bourquet',
-    selectedVariant: 0,
-    altText: "Bouquet of pink and white flowers",
-    toolTip: "Bouquet of pink and white flowers",
-    description: 'Bouquet of flowers perfect for speacial occasions or as an everyday gift.',
-    details: ["1 white Lily", "5 white Carnations", "2 pink Gerbera Daisys", "6 pink Carnations", "Complementary greens"],
-    variants: [{
-      variantId: 01,
-      variantColor: 'MediumVioletRed',
-      variantImage: './img/blumen-pink.jpg',
-      variantAltText: "Bouquet of pink and white flowers",
-      variantToolTip: "Bouquet of pink and white flowers",
-      variantDetails: ["1 white Lily", "5 white Carnations", "2 pink Gerbera Daisys", "6 pink Carnations", "Complementary greens"],
-      variantOnSale: false,
-      variantInStock: true,
-      variantQuantity: 3
-    }, {
-      variantId: 02,
-      variantColor: 'MediumPurple',
-      variantImage: './img/blumen-lila.jpg',
-      variantAltText: "Bouquet of purple flowers",
-      variantToolTip: "Bouquet of purple flowers",
-      variantDetails: ["6 Agapanthus", "6 Delphimium", "Purple Viola", "Ivy leafs", "Complementary greens"],
-      variantOnSale: true,
-      variantInStock: false,
-      variantQuantity: 0
-    }],
-    onSale: false,
-    inventory: 5,
-    url: "https://lindade.github.io/",
-    cart: 0,
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `
+})
+
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `
+    <div class="product">
+      <div class="product-image">
+        <img v-bind:src="image" v-bind:alt="altText" v-bind:title="toolTip">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p>{{ description }}</p>
+        
+        <!-- vue component -->
+        <product-details :details="details"></product-details>
+        
+        <span v-show="onSale">On Sale!</span>
+        
+        <p v-if="inStock > 10" :class="[inStock ? 'in-stock-color' : '']">In Stock</p>
+        <p v-else-if="inStock <= 10 && inStock > 0" class="almost-out-of-stock-color">
+        Almost sold out. Only {{ inStock }} remaining.
+        </p>
+        <p v-else :class="[inStock ? '' : 'out-of-stock-color']">Out of Stock</p>
+        
+        <p>Shipping {{ shipping }}</p>
+        
+        <div v-for="(variant, index) in variants" v-bind:key="variant.variantId" class="color-box"
+        v-bind:style="{ backgroundColor: variant.variantColor }" @mouseover="updateProduct(index)">
+        </div>
+        
+        <button v-on:click="addToCart" :disabled="!inStock" :class="{ 'button:disabled': !inStock }">Add to
+        Cart</button>
+        
+        <button v-show="cart >= 1" v-on:click="removeFromCart">Remove from Cart</button>
+        
+        <div class="cart">
+          <p>Cart ({{ cart }})</p>
+        </div>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      brand: 'Lindas',
+      product: 'Flower Bourquet',
+      selectedVariant: 0,
+      altText: "Bouquet of pink and white flowers",
+      toolTip: "Bouquet of pink and white flowers",
+      description: 'Bouquet of flowers perfect for speacial occasions or as an everyday gift.',
+      details: ["1 white Lily", "5 white Carnations", "2 pink Gerbera Daisys", "6 pink Carnations", "Complementary greens"],
+      variants: [{
+        variantId: 01,
+        variantColor: 'MediumVioletRed',
+        variantImage: './img/blumen-pink.jpg',
+        variantAltText: "Bouquet of pink and white flowers",
+        variantToolTip: "Bouquet of pink and white flowers",
+        variantDetails: ["1 white Lily", "5 white Carnations", "2 pink Gerbera Daisys", "6 pink Carnations", "Complementary greens"],
+        variantOnSale: false,
+        variantInStock: true,
+        variantQuantity: 3
+      }, {
+        variantId: 02,
+        variantColor: 'MediumPurple',
+        variantImage: './img/blumen-lila.jpg',
+        variantAltText: "Bouquet of purple flowers",
+        variantToolTip: "Bouquet of purple flowers",
+        variantDetails: ["6 Agapanthus", "6 Delphimium", "Purple Viola", "Ivy leafs", "Complementary greens"],
+        variantOnSale: true,
+        variantInStock: false,
+        variantQuantity: 0
+      }],
+      onSale: false,
+      inventory: 5,
+      url: "https://lindade.github.io/",
+      cart: 0,
+    }
   },
   methods: {
     addToCart: function () {
@@ -77,6 +135,19 @@ var app = new Vue({
     },
     inStock() {
       return this.variants[this.selectedVariant].variantQuantity
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free"
+      }
+      return 2.99
     }
   }
-});
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    premium: true
+  }
+})
